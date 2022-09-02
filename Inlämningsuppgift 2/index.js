5c85840ecd18dd2a874d4f736a8b73f0d17b7bb3
PhPersson <51906227+PhPersson@users.noreply.github.com>
2022-09-02 09:49:31 +0200

    Inlämningsuppgift 2 - klar

--- Inlämningsuppgift 2/index.js
+++ Inlämningsuppgift 2/index.js
@@ -22,7 +22,6 @@ function min(a, b) {
 
 
 
-
 //Uppgift 2
 function range(n) {
     var array = [];
@@ -61,3 +60,80 @@ function countCharacter(string, charToFind) {
 }
 
 //Uppgift 5
+function plaindrome(string) {
+    var reversedString = string.split("").reverse().join("");
+    if (string != reversedString) {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+//Uppgift 6
+var person = {
+    firstname: "Philip",
+    lastname: "Persson",
+    age: 25,
+    family: ["Lars"]
+};
+
+
+//Uppgift 7
+function printPerson(person) {
+    console.log("Fullname:", person.firstname, person.lastname, "Age:", person.age);
+    for(var i = 0; i < person.family.length; i++) {
+        console.log(person.family[i])
+    }
+}
+printPerson(person)
+
+
+//Uppgift 8
+function createBox(height, width) {
+    var box = {
+        height : height,
+        width : width
+    }
+    return box;
+}
+var box = createBox(15, 20);
+
+console.log(box.height); 
+console.log(box.width);
+
+
+//Uppgift 9
+function Triangle(height, width) {
+    var triangle = {
+        height : height,
+        width : width,
+        area: function(){
+            return this.height * this.width / 2;
+        }
+    }
+    return triangle;
+}
+var tri = Triangle(12, 14);
+console.log(tri.height);
+console.log(tri.width); 
+
+console.log(tri.area());
+
+//Uppgift 10
+function attributes(object) {
+    var attributesArray = []
+    for (var attribute in object) {
+        attributesArray.push(attribute)
+    }
+    return attributesArray;
+}
+
+var testObject2 = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+ 
+ var attributesObj = attributes(testObject2)
+ console.log(attributesObj)
