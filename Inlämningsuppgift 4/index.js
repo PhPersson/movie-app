c343c84866efa4dd6176a6da811d7506bde617c0
PhPersson <51906227+PhPersson@users.noreply.github.com>
2022-09-03 22:04:55 +0200

    Uppg 4 klar

--- Inlämningsuppgift 4/index.js
+++ Inlämningsuppgift 4/index.js
@@ -1,2 +1,63 @@
 "use strict";
 // Philip Persson al4570
+
+
+var searchForm = document.getElementById("search-form");
+var resultDisplayer = document.getElementById("result");
+searchForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var searchText = this.query.value; //För att få ut strängen som användaren söker på
+    if (searchText) {
+        fetchMovie(searchText);
+    } else {
+        alert("U need to enter a title to search for!")
+    }
+});
+
+
+
+function fetchMovie(movieToSeachFor) {
+
+    var apikey = '879e41a8';
+    resultDisplayer.innerHTML = '';
+    var apiRequest = new XMLHttpRequest();
+
+    var omdbAPIUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=" + apikey + "&s="+movieToSeachFor+"&type=movie";
+
+    apiRequest.addEventListener("load", function() {
+
+        var result = JSON.parse(this.responseText);
+        
+        console.log(result)
+        if(result.Response != "False") {
+            handleResponse(result)
+         } else {
+            alert("Couldn't find that movie! \nThe error was: "+ result.Error);
+        }
+
+    });
+    apiRequest.open("GET", omdbAPIUrl, true);
+    apiRequest.send();
+
+
+}
+
+
+function handleResponse(response) {
+    response.Search.map(function(movie) {
+            createDiv(movie);
+    })
+}
+
+
+
+function createDiv(movie) {
+    resultDisplayer.innerHTML += `
+    <div id="movieDiv" "class="card">
+                <div class="container">
+                <h4><b>${movie.Title}</b></h4>
+                <p>${movie.Year}</p>
+                <img src="${movie.Poster}">
+            </div>
+    </div>`
+}
