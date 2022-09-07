12784c0ef62b07dc8bebfab750db3c268b9ad486
PhPersson <51906227+PhPersson@users.noreply.github.com>
2022-09-07 20:55:46 +0200

    started uppg6

---
+++ Inlämningsuppgift 6/src/index.js
@@ -0,0 +1,34 @@
+// Philip Persson al4570
+"use strict"; 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const raceScheduleApi = "https://ergast.com/api/f1/current"
+
+function raceTable(props) {
+  return (
+    <div>
+        <table>
+          <thead>
+
+          </thead>
+        </table>
+
+
+    </div>
+  )
+}
+    
+
+
+
+
+ReactDOM.render(
+  <raceTable />,
+
+  document.getElementById("root")
+);
+
+
