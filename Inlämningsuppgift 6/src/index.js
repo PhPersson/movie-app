4ea2751f009de6380b889bb725322ce6cd64f0d0
PhPersson <51906227+PhPersson@users.noreply.github.com>
2022-09-14 23:04:13 +0200

    not it works, just some css left

--- Inlämningsuppgift 6/src/index.js
+++ Inlämningsuppgift 6/src/index.js
@@ -3,18 +3,25 @@
 import {useState} from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Button from 'react-bootstrap/Button';
+
 
 
 
 const MovieApp = () => {
     
-    const [movies, setMovies] = useState([]);
+    const [gifPhotos, setGifPhotos] = useState([ {
+      link: "https://giphy.com/embed/l0HlDVrHtCP41pDfW"
+    }
+
+    ])
     
+    const [movies, setMovies] = useState([]);
+    // const [gifs, setGifs] = useState({});
     const [searchQuery, setsearchQuery] = useState('');
 
     const getMoviesFromApi = async (searchMovie) => {
       setMovies([]); // För att sökninen skall rensa listan med filmer
+
       var omdbAPIUrl = `https://www.omdbapi.com/?apikey=41059430&type=movie&s=${searchMovie}` // Hämtar data från omdb APi om filmen som användaren valt att söka på
       var omdbAPIUrlResponse = await (await fetch(omdbAPIUrl)).json();
       
@@ -41,19 +48,38 @@ const MovieApp = () => {
       }
     }
 
+    // Funktion för att visa alert meddelanden.
     const showErrorMessage = (message) => {
       alert(message);
     }
 
+    const searchForGiph = async (query) => {
+      var giphyApi = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=cw40uymBW25iT9nKmJhp1M1TTaPI0EIx&limit=1`;
+      var giphyApiResponse = await (await fetch(giphyApi)).json();
+
+
+
+      if (handleApiRes(giphyApiResponse.data)) {
+        setGifPhotos(giphyApiResponse.data)
+      } else {
+        showErrorMessage("Kunde inte generera GIF")
+      }
+
+    }
+
+    const resetGifs = () => {
+      setGifPhotos([]);
+    }
 
     return (
-      <><div></div><div>
-        <SearchBar handleSearchButtonClick = {handleSearchButtonClick} setsearchQuery = {setsearchQuery}/>
-        <ListOfMovies movies={movies} />
-      </div></>
+      <>
+      <div>
+        <SearchBar resetGifs ={resetGifs} handleSearchButtonClick = {handleSearchButtonClick} setsearchQuery = {setsearchQuery}/>
+        <ListOfMovies  gif={gifPhotos} movies={movies} GIFS = {searchForGiph} />
+      </div>
+      </>
     )
 }
-
 
 const SearchBar = (probs) => {
   return (<div>
@@ -65,24 +91,68 @@ const SearchBar = (probs) => {
       value={probs.value}
       onChange = {(event) => probs.setsearchQuery(event.target.value)} //Uppdatera MovieApp.setSearchQuery varje gång en bokstav ändras i sökfältet
       ></input>
-      <button onClick={probs.handleSearchButtonClick}>Sök!</button>
+      <button  className='searchBtn' onClick = {probs.handleSearchButtonClick}>Sök!</button>
+
       </form>
     </header>
     </div>
   );
 }
 
-const ListOfMovies = (probs) => {
+
+class Iframe extends React.Component {
+  render() {
+    
+    return(   
+      <div>          
+        <iframe src={this.props.src}/>         
+      </div>
+    )
+  }
+}
+
+const ListOfMovies = (probs) => {  
+
+  const [isOpen, setIsOpen] = useState(false);
+  const Modal = ({ setIsOpen }) => {
+
+    return (
+      <>
+        <div onClick={() => setIsOpen(false)} />
+        <div >
+          <div>
+
+            <button onClick={() => {setIsOpen(false)} } >
+              X
+            </button>
+              <div>
+                <Iframe src={probs.gif[0].embed_url} />
+              </div>
+            <div >
+
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+
     return (
         <>
+
         {probs.movies.map((movie, index) => (
-            <div className='ImageCard'>
-              <h4><b>{movie.Title}</b></h4>
-              <img src={movie.Poster} alt={movie.Title}></img>
-              <p>Release year: {movie.Year}</p>
-              <button className='memeButton'>Generate a gif! </button>
+            <><div className='ImageCard'>
+            <h4><b>{movie.Title}</b></h4>
+            <img src={movie.Poster} alt={movie.Title}></img>
+            <p>Release year: {movie.Year}</p>
+            <button className='gifButton' onClick={() => { setIsOpen(true); probs.GIFS(movie.Title); } }>Generate a gif! </button>
+            {isOpen && <Modal setIsOpen={setIsOpen} />}
+            <div>
             </div>
-        
+          </div>
+          </>
+
         ))
         }
         </>
@@ -90,13 +160,12 @@ const ListOfMovies = (probs) => {
 }
 
 
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <MovieApp />
-  </React.StrictMode>
+
 );
+
+
+
